a75ce5612e071032bca056bed248d41e133762e7
Beata <becia09269@wp.pl>
2018-10-23 01:12:49 +0200

    class with functions: wordsCount(), wordsFront(), greenTicket()

---
+++ Consileon.java
@@ -0,0 +1,54 @@
+public class Consileon {
+
+    public int wordsCount(String[] words, int len) {
+
+        int howManyWords = 0;
+        for (String word : words)
+        {
+            int howManyLetters = 0;
+            for (char letter : word.toCharArray())
+            {
+                howManyLetters++;
+                if (howManyLetters > len)
+                {
+                    break;
+                }
+            }
+            if (howManyLetters == len)
+            {
+                howManyWords++;
+            }
+        }
+
+        return howManyWords;
+    }
+
+    public String[] wordsFront(String[] words, int n) {
+        String [] stringN = new String[n];
+        for (int i = 0; i < n; i++)
+        {
+            stringN [i] = words [i];
+        }
+
+        return stringN;
+    }
+
+    public int greenTicket(int a, int b, int c) {
+
+        if (a == b)
+        {
+            if (a == c)
+            {
+                return 20;
+            }
+            return 10;
+        }
+
+        if (a == c || b == c)
+        {
+            return 10;
+        }
+
+        return 0;
+    }
+}
